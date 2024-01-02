4c6073b61d508001f714e308359b4525cf1d9737
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2024-01-02 11:58:32 -0300

    first version on console

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-console.log("Hello World")
+
 
 let rock = "ROCK";
 let paper = "PAPER";
 let scissors = "SCISSORS";
+let playerChoicePrompt = prompt("What is your choice?");
+let playerChoice = playerChoicePrompt.slice(0).toLowerCase();
 
 function playerChoiceFunction() {
-    let playerChoice = prompt("What is your choice?");
-    return playerChoice.toLowerCase();
-    if (playerChoice != rock || paper || scissors){
-        console.log("Not a valid choice");
+    if (playerChoice == 'rock'){
+        return rock;
     }
-    else if (playerChoice = rock){
-        console.log("You picked rock!");
+    else if (playerChoice == 'scissors'){
+        return scissors;
+    }
+    else if (playerChoice == 'paper'){
+        return paper;
+    }
+    else {
+        console.log("Not a valid option, pick rock, paper or scissors");
     }
 }
-
+console.log(playerChoiceFunction()); 
+console.log("PLAYER CHOICE ABOVE");
 function getComputerChoice() {
    let choice = Math.floor(Math.random()*3);
    if(choice === 0) {
@@ -24,4 +31,29 @@ function getComputerChoice() {
    } else (choice === 2)
     return scissors;
 }
+console.log(getComputerChoice());
+console.log("COMPUTER CHOICE ABOVE");
+let computerSelection = getComputerChoice();
+let playerSelection = playerChoiceFunction();
+function realGame(computerSelection, playerSelection){
+    if (computerSelection == playerChoice) {
+        console.log("It's a TIE!");
+        return playerChoicePrompt;
+    } else if (computerSelection === rock && playerSelection === paper){
+        return console.log("Player WINS!");
+    } else if (computerSelection === rock && playerSelection === scissors){
+        return console.log("Computer WINS!");
+    } else if (computerSelection === paper && playerSelection === rock){
+        return console.log("Computer WINS!");
+    } else if (computerSelection === scissors && playerSelection === rock){
+        return console.log("Player WINS!");
+    } else if (computerSelection === scissors && playerSelection === paper){
+        return console.log("Computer WINS!");
+    } else if (computerSelection === paper && playerSelection === scissors)
+        return console.log("Player WINS!");
+        else {
+            return
+        }
+}
+realGame(computerSelection, playerSelection);
 

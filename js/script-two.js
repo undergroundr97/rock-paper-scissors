8cad345dbd91c4ca688fc65d6784005db4362977
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2024-01-26 18:15:13 -0300

    added buttons, working as intended, fixed functions, dom is displaying results

--- js/script-two.js
+++ js/script-two.js
@@ -4,27 +4,16 @@ let scissors = "scissors";
 let userScore = parseInt(0);
 let computerScore = parseInt(0);
 
-let match = function (){
-
 function getComputerChoice() {
-   let choice = Math.floor(Math.random()*3);
-   if(choice == 0) {
-    return rock;
-   } else if (choice == 1) {
-    return paper;
-   } else (choice == 2)
-    return scissors;
-}
+    let choice = Math.floor(Math.random()*3);
+    if(choice == 0) {
+     return rock;
+    } else if (choice == 1) {
+     return paper;
+    } else (choice == 2)
+     return scissors;
+ }
 
-let playerSeleciton = playerChoice;
-let computerSelection = getComputerChoice();
-console.log(playerSeleciton, " - Player");
-console.log(computerSelection, " - Computer");
-    return {
-        "playerSelection": playerSeleciton,
-        "computerSelection": computerSelection
-    }
-}
 
 function playRound(playerSeleciton, computerSelection){
     if(playerSeleciton == rock && computerSelection == scissors){
@@ -40,45 +29,50 @@ function playRound(playerSeleciton, computerSelection){
    } else if (playerSeleciton == scissors && computerSelection == rock){
         return computerScore++, console.log("You lose, rock beats scissors.");
    } else if (playerSeleciton == computerSelection){
-        return ("Its a tie pick again");
+        return console.log("Its a tie pick again");
     } else (playerSeleciton == null)
         return console.log("Pick a valid selection");
 }
 
-function game(){
-        let repeat = match();
-        playRound(repeat.playerSelection, repeat.computerSelection);
-}  
 
 function winner(){
-    if(userScore >= 3){
-        return alert(`YOU WINT WITH ${userScore} POINTS!`), console.log('You are the winner!')
-    } else (computerScore >= 3)
+    if(userScore == 5){
+        return alert(`YOU WIN WITH ${userScore} POINTS!`), console.log('You are the winner!')
+    } else if (computerScore == 5)
         return alert(`COMPUTER WINS WITH ${computerScore} POINTS!`), console.log('Computer is the winner')
+        else {
+            return 
+        }
 }
 
-console.log(userScore, " - This is your score");
-console.log(computerScore, " - This is computer score");
-
-const scoreBoard = document.querySelector('#play');
+const section = document.querySelector('section');
+const divScore = document.createElement('div')
+section.appendChild(divScore);
 const rockButton = document.createElement('button');
 const paperButton = document.createElement('button');
 const scissorsButton = document.createElement('button');
 paperButton.textContent = "PAPER!";
 scissorsButton.textContent= "SCISSORS!";
 rockButton.textContent = "ROCK!";
-scoreBoard.appendChild(rockButton);
-scoreBoard.appendChild(paperButton);
-scoreBoard.appendChild(scissorsButton);
+divScore.appendChild(rockButton);
+divScore.appendChild(paperButton);
+divScore.appendChild(scissorsButton);
+const scoreBoard = document.createElement('p');
+scoreBoard.textContent = `You won ${userScore} times and Computer won ${computerScore} times.`
+section.appendChild(scoreBoard);
 
 rockButton,addEventListener('click', () => {
-    return playerChoice = rock, game();
+    return playerSelection = rock, playRound(playerSelection, getComputerChoice()), scoreBoard.textContent = `You won ${userScore} times and Computer won ${computerScore} times.`, winner();
 })
 
 scissorsButton.addEventListener('click', () => {
-    return playerChoice = scissors, game();
+    return playerSelection = scissors, playRound(playerSelection, getComputerChoice()), scoreBoard.textContent = `You won ${userScore} times and Computer won ${computerScore} times.`, winner();
 })
 
 paperButton.addEventListener('click', () => {
-    return playerChoice == paper, playRound();
+    return playerSelection = paper, playRound(playerSelection, getComputerChoice()), scoreBoard.textContent = `You won ${userScore} times and Computer won ${computerScore} times.`, winner();
 })
+
+console.log(userScore, " - This is your score");
+console.log(computerScore, " - This is computer score");
+
